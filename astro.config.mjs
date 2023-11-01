6bf430ec41d7144ef2ec97e28a0079a67bf77d89
juan <juan.villegas.dev@gmail.com>
2023-11-01 13:55:11 -0500

    core:add site propertie

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  site : "https://ilovejs.org",
+  integrations: [tailwind(), react(), sitemap()],
 });
