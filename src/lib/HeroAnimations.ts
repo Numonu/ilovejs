9c4b54b187b7767edac94babf1501ade4e07c01b
juan <juan.villegas.dev@gmail.com>
2023-11-13 15:04:18 -0500

    feat:prevent layout shifting

--- src/lib/HeroAnimations.ts
+++ src/lib/HeroAnimations.ts
@@ -1,7 +1,18 @@
 import gsap from "gsap";
 
 const title_tl = gsap.timeline();
+title_tl.pause();
 
+// prevent layout shifting
+
+const container = document.querySelector(".container") as HTMLElement;
+addEventListener("load" , () => {
+    if (container) {
+        container.style.visibility = "inherit";
+        title_tl.play();
+    }
+
+})
 
 // title reveal
 
@@ -58,49 +69,60 @@ const title_tl = gsap.timeline();
     };
 
     const endLoop_tl = gsap.timeline({
-        repeat : -1,
+        repeat: -1,
     });
 
     endLoop_tl.to(target, from, "<10%");
     endLoop_tl.to(target, to, "<50%");
 }
 
-
 // description reveal
 
-title_tl.from(".description" , {
-    opacity : 0,
-    y : 45,
-    x : 45,
-    scale : 1.2,
-    rotateY : "90deg",
-    skewX : "35deg",
-} , "<90%")
+title_tl.from(
+    ".description",
+    {
+        opacity: 0,
+        y: 45,
+        x: 45,
+        scale: 1.2,
+        rotateY: "90deg",
+        skewX: "35deg",
+    },
+    "<90%"
+);
 
-title_tl.addLabel("description" , ">");
+title_tl.addLabel("description", ">");
 
 // icon reveal
 
-title_tl.from(".js" , {
-    // x : "+=100",
-    y : "-=100",
-    
-    scale : 0,
-    // rotateZ : "180deg",
-    stagger : .1
-}, 0)
+title_tl.from(
+    ".js",
+    {
+        // x : "+=100",
+        y: "-=100",
 
-title_tl.from(".crown" , {
-    y : "-=50",
-    scale : 1.2,
-    duration : .1,
-    opacity : 0,
-}, "description")
+        scale: 0,
+        // rotateZ : "180deg",
+        stagger: 0.1,
+    },
+    0
+);
 
-title_tl.to(".js" , {
-    y : "+=5",
-    repeat : 1,
-    yoyo : true,
-    duration : .1,
-    ease : "none"
-})
+title_tl.from(
+    ".crown",
+    {
+        y: "-=50",
+        scale: 1.2,
+        duration: 0.1,
+        opacity: 0,
+    },
+    "description"
+);
+
+title_tl.to(".js", {
+    y: "+=5",
+    repeat: 1,
+    yoyo: true,
+    duration: 0.1,
+    ease: "none",
+});
